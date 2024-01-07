9657a87c7d349e0d91fa27815fac8f2f03e59719
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2024-01-06 22:10:51 -0400

    ready to deploy

--- artpulse/settings.py
+++ artpulse/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tq9bgcyyh%*z%be^&x^r)g=hg%j8*ymuxilkj_yp&xqd&kc^yf'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,8 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
     'home',
-    'artpulse'
+    'artpulse',
 ]
 
 MIDDLEWARE = [
--- home/views.py
+++ home/views.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-# Add this line of code for all your URLs (i.e about.html, contact.html, etc.)
     path('', views.index, name='index'),
 ]
