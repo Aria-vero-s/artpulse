ba860fb86c12299849c985f9fa65f511b0d5d648
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2024-01-09 15:15:15 -0400

    removed static_root

--- artpulse/settings.py
+++ artpulse/settings.py
@@ -174,7 +174,7 @@ if 'USE_AWS' in os.environ:
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
