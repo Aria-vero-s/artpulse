7773283510550739c0aa5612bf7e9931d4785066
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2024-01-09 15:40:19 -0400

    removded whitenoise and cloudinary

--- artpulse/settings.py
+++ artpulse/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', #added for heroku deployment
 ]
 
 ROOT_URLCONF = 'artpulse.urls'
