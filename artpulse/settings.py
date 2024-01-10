920f020f19ad7a24da6f592814945e0039271218
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2024-01-09 21:13:42 -0400

    changed bucket region

--- artpulse/settings.py
+++ artpulse/settings.py
@@ -180,6 +180,10 @@ if 'USE_AWS' in os.environ:
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
+    STATIC_URL = '/static/'
+    STATIC_MEDIA = '/media/'
+    https://artpulse.s3.amazonaws.com/static/image.jpeg'
+
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
